84cf1e260fcd6f1d29684937d5ca7f61a7d77a3d
Yajima <119467077+jajimajp@users.noreply.github.com>
2024-07-09 16:59:10 +0900

    Implement add todo

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,38 @@
-import 'https://unpkg.com/htm@3.1.1/dist/htm.js';
-import 'https://unpkg.com/preact@10.22.1/dist/preact.min.module.js';
+import { h, Component, render } from 'https://unpkg.com/preact@latest?module';
+import { useState } from 'https://unpkg.com/preact@latest/hooks/dist/hooks.module.js?module';
+import htm from 'https://unpkg.com/htm@3.1.1/dist/htm.module.js?module';
+const html = htm.bind(h);
 
-const html = htm.bind(preact.h);
-const App = () => html`<h1>Hello, world!</h1>`;
-preact.render(html`<${App} />`, document.body);
+const initialTodos = [
+  { id: 1, name: 'First task' },
+  { id: 2, name: 'Second task' },
+  { id: 3, name: 'Third task' },
+];
+
+const App = () => {
+  const [todos, setTodos] = useState(initialTodos);
+  const [newTodo, setNewTodo] = useState('');
+  const handleChange = (e) => setNewTodo(e.target.value);
+  const handleClick = () => {
+    if (newTodo !== '') {
+      const name = newTodo;
+      setTodos([...todos, { id: todos.length + 1, name }]);
+      setNewTodo('');
+    }
+  };
+
+  return html`
+    <div>
+      <h1>ToDos</h1>
+      <input type="text" value=${newTodo} onChange=${handleChange} />
+      <button onClick=${handleClick}>Add</button>
+      <ul>
+        ${todos.map((todo) => html`
+          <li key=${todo.id}>${todo.name}</li>
+        `)}
+      </ul>
+    </div>
+  `;
+}
+
+render(html`<${App} />`, document.body);
